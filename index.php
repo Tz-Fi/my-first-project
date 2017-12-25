befa567b8ab77cf3aa2dc78b209ba7d2ea047359
Tz-Fi <tzviefiller@gmail.com>
2017-12-24 22:11:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
       Title Goes Here!
     </title>
     <style>
+      body{
+        background-color:blue;
+        align:center;
+        text-align:center;
+      }
     </style>
   </head>
   
   <body>
+    <a href=page1.php>page 1</a>
+    <br />
+    <a href=page2.php>page 2</a>
+    <br />
   </body>
 
 </html>
