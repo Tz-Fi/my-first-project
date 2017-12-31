1613a672ab5a4d81cb2ec4c20c455dcdb484f399
Tz-Fi <tzviefiller@gmail.com>
2017-12-31 17:54:51 -0500

    Update game3.js

--- game3.js
+++ game3.js
@@ -3,17 +3,17 @@ var set_game = function(){
   for(var i = 0; i <= 4; i++){
     $("#div1").append("<tr id=row"+i+" >");
     for(var j = 0; j <= 4; j++){
-      $("#div1").append("<td id=row"+i+"column"+j+" >"+i+"x"+j+"</td>");
+      $("#div1").append("<td id=row"+i+"column"+j+" >"+i+"x"+j+" </td>");
     }
     $("#div1").append("</tr>");
   }
-  $("#div1").append("</table>");
+  $("#div1").append("</table><br />");
   
   $("#div1").append("<table id=defender style='width:100%' >");
   for(var i = 0; i <= 4; i++){
     $("#div1").append("<tr id=row"+i+" >");
     for(var j = 0; j <= 4; j++){
-      $("#div1").append("<td id=row"+i+"column"+j+" >"+i+"x"+j+"</td>");
+      $("#div1").append("<td id=row"+i+"column"+j+" >"+i+"x"+j+" </td>");
     }
     $("#div1").append("</tr>");
   }
