4b55be186076c9e482f687e5e51eab54d5ab6616
Tz-Fi <tzviefiller@gmail.com>
2017-12-31 18:21:36 -0500

    Update game3.js

--- game3.js
+++ game3.js
@@ -7,24 +7,23 @@ var bomb = function(i,j){
 var set_game = function(){
   $("#div1").append("<table id=target style='margin-left:auto;margin-right:auto;' >");
   for(var i = 1; i <= 5; i++){
-    $("#div1").append("<tr id=row"+i+" >");
+    $("#target").append("<tr id=row"+i+" ></tr>");
     for(var j = 1; j <= 5; j++){
-      $("#div1").append("<td id=row"+i+"column"+j+" ><button id=btnrow"+i+"column"+j+">"+i+"x"+j+" </button></td>");
+      $("#row"+i).append("<td id=row"+i+"column"+j+" ><button id=btnrow"+i+"column"+j+">"+i+"x"+j+" </button></td>");
       let m = i;
       let n = j;
       $("#btnrow"+i+"column"+j).click(function(){
         bomb(m,n);
       });  
     }
-    $("#div1").append("</tr>");
   }
   $("#div1").append("</table><br />");
   
   $("#div1").append("<table id=defender style='margin-left:auto;margin-right:auto;' >");
   for(var i = 1; i <= 5; i++){
-    $("#div1").append("<tr id=row"+i+" >");
+    $("#defender").append("<tr id=drow"+i+" >");
     for(var j = 1; j <= 5; j++){
-      $("#div1").append("<td id=row"+i+"column"+j+" ><button id=btnrow"+i+"column"+j+">"+i+"x"+j+" </button></td>");
+      $("#drow"+i).append("<td id=row"+i+"column"+j+" ><button id=btnrow"+i+"column"+j+">"+i+"x"+j+" </button></td>");
     }
     $("#div1").append("</tr>");
   }
