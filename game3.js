8a71e13a91c8d4285555252cb2eae7d1dd5de6ad
Tz-Fi <tzviefiller@gmail.com>
2017-12-31 17:41:36 -0500

    Create game3.js

---
+++ game3.js
@@ -0,0 +1,21 @@
+var set_game = function(){
+  $(#div1).append("<table id=target style="width:50%">");
+  for(var i = 0; i <= 4; i++){
+    $(#div1).append("<tr id=row"+i+" >");
+    for(var j = 0; j <= 4; j++){
+      $(#div1).append("<td id=row"+i+"column"+j+" >"+i+"x"+j+"</td>");
+    }
+    $(#div1).append("</tr>");
+  }
+  $(#div1).append("</table>");
+  
+  $(#div1).append("<table id=defender style="width:100%" >");
+  for(var i = 0; i <= 4; i++){
+    $(#div1).append("<tr id=row"+i+" >");
+    for(var j = 0; j <= 4; j++){
+      $(#div1).append("<td id=row"+i+"column"+j+" >"+i+"x"+j+"</td>");
+    }
+    $(#div1).append("</tr>");
+  }
+  $(#div1).append("</table>");
+}
