3775b3a013ffd8866cbcc43455068c388212120f
Tz-Fi <tzviefiller@gmail.com>
2017-12-31 17:58:50 -0500

    Update game3.js

--- game3.js
+++ game3.js
@@ -1,19 +1,19 @@
 var set_game = function(){
   $("#div1").append("<table id=target style='width:50%'>");
-  for(var i = 0; i <= 4; i++){
+  for(var i = 1; i <= 5; i++){
     $("#div1").append("<tr id=row"+i+" >");
     for(var j = 0; j <= 4; j++){
-      $("#div1").append("<td id=row"+i+"column"+j+" >"+i+"x"+j+" </td>");
+      $("#div1").append("<td id=row"+i+"column"+j+" ><button id=btnrow"+i+"column"+j+">"+i+"x"+j+" </button></td>");
     }
     $("#div1").append("</tr>");
   }
   $("#div1").append("</table><br />");
   
   $("#div1").append("<table id=defender style='width:100%' >");
-  for(var i = 0; i <= 4; i++){
+  for(var i = 1; i <= 5; i++){
     $("#div1").append("<tr id=row"+i+" >");
     for(var j = 0; j <= 4; j++){
-      $("#div1").append("<td id=row"+i+"column"+j+" >"+i+"x"+j+" </td>");
+      $("#div1").append("<td id=row"+i+"column"+j+" ><button id=btnrow"+i+"column"+j+">"+i+"x"+j+" </button></td>");
     }
     $("#div1").append("</tr>");
   }
